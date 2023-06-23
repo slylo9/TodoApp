ece1f4a5204f4eb74ef7900f11e9381880b98d0c
Stewart Mckenzie <stewart.develops@gmail.com>
2023-06-23 17:40:14 -0500

    Add project files.

---
+++ TodoApp.Frontend/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TodoApp.Frontend.Models;
+
+namespace TodoApp.Frontend.Controllers
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Todos = new List<Todo> { new Todo { Id=1,
+                Name="Fix the APIs"}, new Todo { Id=2,
+                Name="hen try again"}, };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
